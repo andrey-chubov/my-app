6a67a4cf242227ca2ab2d948b8e8847257771c66
Andrey Chubov <113904877+andrey-chubov@users.noreply.github.com>
2024-03-01 08:44:11 +0300

    Update ui.tsx

--- src/pages/list-books/ui.tsx
+++ src/pages/list-books/ui.tsx
@@ -8,7 +8,7 @@ import { Loading } from "shared/ui/loading";
 import { BooksListWidget, BooksFooterWidget } from "widgets/book";
 
 export const BooksScreen: FC<ScreenProps<"Books">> = () => {
-  const { isLoading, error, data, refetch } = bookApi.useFetchAllBooksQuery(10);
+  const { isLoading, error, data, refetch } = bookApi.useFetchAllBooksQuery();
 
   if (isLoading) return <Loading />;
 
